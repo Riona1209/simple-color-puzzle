2cb5172fb2bff5515d0e81d8d7ce6b98dadf96a1
Riona1209 <154750938+Riona1209@users.noreply.github.com>
2024-04-04 10:24:35 +0800

    feat: change log

--- git_change.sh
+++ git_change.sh
@@ -1,10 +1,10 @@
 git filter-branch --env-filter '
-      if test "$GIT_AUTHOR_EMAIL" = "35563498+SaiKiranVaddi@users.noreply.github.com"
+      if test "$GIT_AUTHOR_EMAIL" = "154750938+Riona1209@users.noreply.github.com"
       then
               GIT_AUTHOR_EMAIL=riona1209@outlook.com
               GIT_AUTHOR_NAME="Vegetable_builder"
       fi
-      if test "$GIT_COMMITTER_EMAIL" = "35563498+SaiKiranVaddi@users.noreply.github.com"
+      if test "$GIT_COMMITTER_EMAIL" = "154750938+Riona1209@users.noreply.github.com"
       then
               GIT_COMMITTER_EMAIL=riona1209@outlook.com
               GIT_AUTHOR_NAME="Vegetable_builder"
